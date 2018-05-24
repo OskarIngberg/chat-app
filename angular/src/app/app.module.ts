8909a3e69ea4d0b4c5224dbd33ea29e7c3b5a2f3
Oskar Ingberg <oskar.ingberg@claremont.se>
2018-05-24 14:02:48 +0200

    Add angular structure

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './header/search/search.component';
+import { InfoComponent } from './header/info/info.component';
+import { LeftPanelComponent } from './left-panel/left-panel.component';
+import { RightPanelComponent } from './right-panel/right-panel.component';
+import { AddChatComponent } from './add-chat/add-chat.component';
+import { MessagesPanelComponent } from './messages-panel/messages-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    InfoComponent,
+    LeftPanelComponent,
+    RightPanelComponent,
+    AddChatComponent,
+    MessagesPanelComponent
   ],
   imports: [
     BrowserModule
--- api/server.js
+++ api/server.js
@@ -2,10 +2,6 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
@@ -15,6 +11,26 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg) {
     io.emit('chat message', msg);
   });
+});
+
+// Allow Localhost
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
 });
 
 http.listen(3000, function(){
